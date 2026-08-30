381ddbeedc9ddae3e974ae941e6b2095040ff79d
Ricard Cabezas <r.cabezas.diez@gmail.com>
2026-08-30 18:38:27 +0200

    refactor(enemies): centralize bee animator constants

---
+++ Assets/Features/Entities/Scripts/Constants/Constants.cs
@@ -0,0 +1,15 @@
+namespace Game.GamePlay.Entities
+{
+	public static class Constants
+	{
+		public static class Animator
+		{
+			public static class Bee
+			{
+				public const string IsMoving = "IsMoving";
+				public const string Damage = "Damage";
+				public const string Attack = "Attack";
+			}
+		}
+	}
+}
